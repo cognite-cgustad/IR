31b4723bae9eb8020905fa3817e7b993821737dd
Christian Gustad <christian.gustad@cognite.com>
2021-06-04 05:57:40 +0200

    Fixed up main

--- src/main.py
+++ src/main.py
@@ -12,46 +12,39 @@ image_paths.sort()
 # Pick baseline image as inital image
 baseline_path = image_paths.pop(0)
 baseline_image = cv2.imread(baseline_path)
-baseline_hls = cv2.cvtColor(baseline_image, cv2.COLOR_BGR2HLS)
-baseline_h, baseline_l, baseline_s =  baseline_hls[:, :, 0], baseline_hls[:, :, 1], baseline_hls[:, :, 2]
 baseline_gray = cv2.cvtColor(baseline_image, cv2.COLOR_BGR2GRAY)
-b_cp = np.copy(baseline_l)
-b_gray = np.copy(baseline_gray)
+b_cp = np.copy(baseline_gray)
+
 
 def ir_analysis(filename, pixel_temperature_threshold):
     img = cv2.imread(filename)
-    # Add black spots
-    for i in range(0, 20):
-        x = np.random.randint(low = 0, high = 300)
-        y = np.random.randint(low = 0, high = 220)
-        p0 = x,y
-        p1 = x+5,y+5
-        cv2.rectangle(img,p0,p1,(0,0,0),cv2.FILLED)
-
-    hls_img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-    h, l, s = hls_img[:, :, 0], hls_img[:, :, 1],  hls_img[:, :, 2]
+    # # Add black spots
+    # for i in range(0, 20):
+    #     x = np.random.randint(low = 0, high = 300)
+    #     y = np.random.randint(low = 0, high = 220)
+    #     p0 = x,y
+    #     p1 = x+5,y+5
+    #     cv2.rectangle(img,p0,p1,(0,0,0),cv2.FILLED)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    cp = np.copy(l)
-    cp_gray = np.copy(img_gray)
+    cp = np.copy(img_gray)
     hot = np.zeros(cp.shape)
+    heat = 0
     cold = np.zeros(cp.shape)
-    alarm_score = 0
+    chill = 0
     for row in range(cp.shape[0]):
         for col in range(cp.shape[1]):
-            # Check tempratue threshold
-            if cp[row][col] > pixel_temperature_threshold:
-                alarm_score += cp[row][col] - pixel_temperature_threshold
             # Get hot increase
-            pixel_delta = cp_gray[row][col] - b_gray[row][col]
+            pixel_delta = cp[row][col] - b_cp[row][col]
             if pixel_delta > 0:
                 hot[row][col] = pixel_delta
+                print(pixel_delta)
             elif pixel_delta < 0:
                 cold[row][col] = pixel_delta
                 print(pixel_delta)
             else:
                 pass
-    return img, cp, alarm_score, hot, cold
+    return img, cp, hot, cold
 
 
 # Plot data
@@ -59,11 +52,10 @@ rows, cols = len(image_paths), 4
 
 for count, path in enumerate(image_paths):
     index = 4*count
-    img, lightness, alarm, hot, cold = ir_analysis(path, 148)
+    img, lightness, hot, cold = ir_analysis(path, 148)
     image_name = path.split('/')[-1]
     hot = np.ma.masked_where(hot == 0, hot)
     cold = np.ma.masked_where(cold == 0, cold)
-    print(f"Alarm score for {image_name} was {alarm}.")
     # Add baseline
     plt.subplot(rows, cols, index+1)
     plt.title(f"Baseline")
