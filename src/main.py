b7ace1dbdf19b9efe42004c273c8b6d90ac904aa
Christian Gustad <christian.gustad@cognite.com>
2021-06-04 06:41:01 +0200

    Fixed up main even more

--- src/main.py
+++ src/main.py
@@ -16,69 +16,67 @@ baseline_gray = cv2.cvtColor(baseline_image, cv2.COLOR_BGR2GRAY)
 b_cp = np.copy(baseline_gray)
 
 
-def ir_analysis(filename, pixel_temperature_threshold):
+def ir_analysis(filename):
+    global cp
     img = cv2.imread(filename)
     # # Add black spots
-    # for i in range(0, 20):
-    #     x = np.random.randint(low = 0, high = 300)
-    #     y = np.random.randint(low = 0, high = 220)
-    #     p0 = x,y
-    #     p1 = x+5,y+5
-    #     cv2.rectangle(img,p0,p1,(0,0,0),cv2.FILLED)
+    for i in range(0, 20):
+        x = np.random.randint(low = 0, high = 300)
+        y = np.random.randint(low = 0, high = 220)
+        p0 = x,y
+        p1 = x+5,y+5
+        cv2.rectangle(img,p0,p1,(5,5,5),cv2.FILLED)
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     cp = np.copy(img_gray)
-    hot = np.zeros(cp.shape)
-    heat = 0
-    cold = np.zeros(cp.shape)
-    chill = 0
+    hot = cold = np.zeros(cp.shape)
+    hotness = coldness = 0
     for row in range(cp.shape[0]):
         for col in range(cp.shape[1]):
-            # Get hot increase
-            pixel_delta = cp[row][col] - b_cp[row][col]
-            if pixel_delta > 0:
-                hot[row][col] = pixel_delta
-                print(pixel_delta)
-            elif pixel_delta < 0:
-                cold[row][col] = pixel_delta
-                print(pixel_delta)
-            else:
+            pixel_delta = cp[row][col].astype(float) - b_cp[row][col].astype(float)
+            if pixel_delta == 0:
                 pass
-    return img, cp, hot, cold
+            elif pixel_delta > 0:
+                hot[row][col] = pixel_delta
+                hotness += pixel_delta
+            else:
+                cold[row][col] = pixel_delta
+                coldness += pixel_delta
+    return img, cp, hot, cold, hotness, coldness
 
 
-# Plot data
-rows, cols = len(image_paths), 4
-
-for count, path in enumerate(image_paths):
-    index = 4*count
-    img, lightness, hot, cold = ir_analysis(path, 148)
-    image_name = path.split('/')[-1]
-    hot = np.ma.masked_where(hot == 0, hot)
-    cold = np.ma.masked_where(cold == 0, cold)
-    # Add baseline
-    plt.subplot(rows, cols, index+1)
-    plt.title(f"Baseline")
-    plt.imshow(cv2.cvtColor(baseline_image, cv2.COLOR_BGR2RGB))
-    plt.axis('off')
-    # Add image
-    plt.subplot(rows, cols, index+2)
-    plt.title(f"{image_name}")
-    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    plt.axis('off')
-    # Add hot
-    plt.subplot(rows, cols, index+3)
-    plt.title(f"Heat")
-    plt.imshow(lightness,cmap="gray")
-    plt.imshow(hot, cmap='hot')
-    plt.axis('off')
-    # Add cold
-    plt.subplot(rows, cols, index+4)
-    plt.title(f"Cold")
-    plt.imshow(lightness,cmap="gray")
-    plt.imshow(cold, cmap='hot')
-    plt.axis('off')
-
-plt.show()
+def make_plot():
+    # Plot data
+    rows, cols = len(image_paths), 4
+    for count, path in enumerate(image_paths):
+        index = 4*count
+        img, lightness, hot, cold, total_heat, total_cool = ir_analysis(path)
+        image_name = path.split('/')[-1]
+        hot = np.ma.masked_where(hot <= 0, hot)
+        cold = np.ma.masked_where(cold >= 0, cold)
+        print(f"Total heat: {total_heat}")
+        print(f"Total cool: {total_cool}")
+        # Add baseline
+        plt.subplot(rows, cols, index+1)
+        plt.title(f"Baseline")
+        plt.imshow(cv2.cvtColor(baseline_image, cv2.COLOR_BGR2RGB))
+        plt.axis('off')
+        # Add image
+        plt.subplot(rows, cols, index+2)
+        plt.title(f"{image_name}")
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        plt.axis('off')
+        # Add hot
+        plt.subplot(rows, cols, index+3)
+        plt.imshow(lightness, cmap="gray")
+        plt.imshow(hot, cmap='hot')
+        plt.axis('off')
+        # Add cold
+        plt.subplot(rows, cols, index+4)
+        plt.title(f"Cold")
+        plt.imshow(lightness, cmap="gray")
+        plt.imshow(cold, cmap='cool')
+        plt.axis('off')
+    plt.show()
 #for fname in ['.jpg', '_1w.jpg', '_2w.jpg', '_3w.jpg']:
 #    print(fname, pixel_temperature_score('/home/luka/dev/cognite/power-demo-apps/infrared.cache/p9085' + fname, 148))
